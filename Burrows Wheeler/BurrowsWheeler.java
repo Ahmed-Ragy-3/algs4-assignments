4f2bef16b75cd17d02796e25e0aeb41f2fdbd391
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2026-02-10 01:06:05 +0200

    update burrows wheeler implementation

--- Burrows Wheeler/BurrowsWheeler.java
+++ Burrows Wheeler/BurrowsWheeler.java
@@ -1,8 +1,8 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.HexDump;
-import edu.princeton.cs.algs4.BinaryIn;
-import edu.princeton.cs.algs4.BinaryOut;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 // import edu.princeton.cs.algs4.Huffman;
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
@@ -10,45 +10,89 @@ import edu.princeton.cs.algs4.BinaryStdOut;
 @SuppressWarnings("unused")
 public class BurrowsWheeler {
 
+	private static final int R = 256;
+
 	private static String sort(String str) {
 		int n = str.length();
-		char[] chs = new char[n];
-		int alphabet = 256;
+		char[] srt = new char[n];
 
-		int[] cnt = new int[Math.max(alphabet, n)];
+		int[] cnt = new int[Math.max(R, n)];
 
-		for (int i = 0; i < n; i++)			cnt[str.charAt(i)]++;
-		for (int i = 1; i < alphabet; i++)	cnt[i] += cnt[i - 1];
-		for (int i = 0; i < n; i++) 			chs[--cnt[str.charAt(i)]] = str.charAt(i);
+		for (int i = 0; i < n; i++)	cnt[str.charAt(i)]++;
+		for (int i = 1; i < R; i++)	cnt[i] += cnt[i - 1];
+		for (int i = 0; i < n; i++) 	srt[--cnt[str.charAt(i)]] = str.charAt(i);
 
-		return new String(chs);
+		return new String(srt);
 	}
 
 	// apply Burrows-Wheeler transform,
 	// reading from standard input and writing to standard output
 	public static void transform() {
-		// String s = BinaryStdIn.readString();
-		String s = StdIn.readString();
+		String s = BinaryStdIn.readString();
 		int n = s.length();
+
 		CircularSuffixArray suffixArray = new CircularSuffixArray(s);
+
+		int first = 0;
+		for (int i = 0; i < n; i++) {
+			if (suffixArray.index(i) == 0) {
+				first = i;
+				break;
+			}
+		}
+		
+		BinaryStdOut.write(first);
+
 		for (int i = 0; i < n; i++) {
 			int j = suffixArray.index(i);
 			BinaryStdOut.write(s.charAt((j == 0 ? n : j) - 1));
-			System.out.print(s.charAt((j == 0 ? n : j) - 1));
 		}
+		BinaryStdOut.close();
 	}
 
 	// apply Burrows-Wheeler inverse transform,
 	// reading from standard input and writing to standard output
 	public static void inverseTransform() {
-		String L = BinaryStdIn.readString();
-		// String F = sort(L)
+		int first = BinaryStdIn.readInt();
+		String t = BinaryStdIn.readString();
+		int n = t.length();
+
+		String srt = sort(t);
+		int[] next = new int[n];
+
+		// Constructing next[] array
+		HashMap<Character, Queue<Integer>> map = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			map.putIfAbsent(t.charAt(i), new LinkedList<>());
+			map.get(t.charAt(i)).add(i);
+		}
+
+		for (int i = 0; i < n; i++) {
+			next[i] = map.get(srt.charAt(i)).poll();
+		}
+		System.out.println("next[]: " + Arrays.toString(next));
+
+		// Original string construction from next[] and srt
+		int cur = first;
+		for (int i = 0; i < n; i++) {
+			BinaryStdOut.write(srt.charAt(cur));
+			cur = next[cur];
+		}
+
+		BinaryStdOut.close();
 	}
 
 	// if args[0] is "-", apply Burrows-Wheeler transform
 	// if args[0] is "+", apply Burrows-Wheeler inverse transform
 	public static void main(String[] args) {
-		BurrowsWheeler.transform();
-		// System.out.println(BurrowsWheeler.sort("ARD!RCAAAABB"));
+		// assert args.length == 1;
+		if (args[0].equals("-"))
+			BurrowsWheeler.transform();
+
+		else if (args[0].equals("+"))
+			BurrowsWheeler.inverseTransform();
+
+		else
+			throw new IllegalArgumentException("Invalid argument");
 	}
 }
--- Burrows Wheeler/MoveToFront.java
+++ Burrows Wheeler/MoveToFront.java
@@ -1,68 +1,65 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.HexDump;
-
 import java.util.LinkedList;
 
-import edu.princeton.cs.algs4.BinaryIn;
-import edu.princeton.cs.algs4.BinaryOut;
 import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
 
 @SuppressWarnings("unused")
 public class MoveToFront {
 
-	private static void fillSequence(LinkedList<Character> sequence) {
-		for (int i = 0; i < 256; i++) {
+	private static final int R = 256;
+
+	private static void fillSequence(LinkedList<Byte> sequence) {
+		assert sequence.isEmpty();
+		for (byte i = 0; i < R; i++) {
 			// ith extended ASCII character
-			sequence.addLast((char) i);
+			sequence.addLast(i);
 		}
 	}
 
 	// apply move-to-front encoding, reading from standard input and
-	// writing to abra.txt
 	public static void encode() {
-		LinkedList<Character> seq = new LinkedList<>();
+		LinkedList<Byte> seq = new LinkedList<>();
 		fillSequence(seq);
 
-		BinaryOut out = new BinaryOut("abra.txt");
-
+		// writing to standard output
 		while (!BinaryStdIn.isEmpty()) {
-			char c = BinaryStdIn.readChar();
+			byte c = (byte) BinaryStdIn.readChar();
 			int idx = seq.indexOf(c);
-			out.write(idx, 8);
+			BinaryStdOut.write(idx, 8);
 			seq.remove(idx);
 			seq.addFirst(c);
 		}
-		out.close();
+
+		BinaryStdOut.close();
 	}
 
 	// apply move-to-front decoding, reading from standard input and
-	// writing to abra.txt
 	public static void decode() {
-		LinkedList<Character> seq = new LinkedList<>();
+		LinkedList<Byte> seq = new LinkedList<>();
 		fillSequence(seq);
 
-		BinaryOut out = new BinaryOut("abra.txt");
-
+		// writing to standard output
 		while (!BinaryStdIn.isEmpty()) {
 			int idx = BinaryStdIn.readChar();
-			char c = seq.get(idx);
-			out.write(c, 8);
+			byte c = seq.get(idx);
+			BinaryStdOut.write(c, 8);
 			seq.remove(idx);
 			seq.addFirst(c);
 		}
-		out.close();
+
+		BinaryStdOut.close();
 	}
 
 	// if args[0] is "-", apply move-to-front encoding
 	// if args[0] is "+", apply move-to-front decoding
 	public static void main(String[] args) {
+		// assert args.length == 1;
 		if (args[0].equals("-"))
 			MoveToFront.encode();
-		
+
 		else if (args[0].equals("+"))
 			MoveToFront.decode();
-		
+
 		else
 			throw new IllegalArgumentException("Invalid argument");
 	}
